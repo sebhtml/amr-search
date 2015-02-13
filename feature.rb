13da625ef48fef6c4e1b262521775870c3bd9bc0
Sébastien Boisvert <boisvert@anl.gov>
2015-02-13 14:05:07 -0600

    add another type of probe name

--- feature.rb
+++ feature.rb
@@ -18,6 +18,7 @@ class Feature
 
         if detect_gene_with_pipe
         elsif detect_gene_with_parenthesis
+        elsif detect_gene_with_number
         end
 
         @gene_name
@@ -48,6 +49,13 @@ class Feature
     # parse this:
     # AB071145:1-1143mac(A)1143_808_842
     def detect_gene_with_parenthesis
+
+        if @probe_name.count(":") != 1
+            return false
+        elsif @probe_name.count("-") != 1
+            return false
+        end
+
         token = @probe_name.split(":")[1]
 
         token = token.split("-")[1]
@@ -68,5 +76,41 @@ class Feature
         end
 
         false
+    end
+
+    # parse this:
+    # AF012101:562-1749vceA1188_701_735
+    def detect_gene_with_number
+        if @probe_name.count(":") != 1
+            return false
+        elsif @probe_name.count("-") != 1
+            return false
+        end
+
+        token = @probe_name.split(":")[1]
+
+        token = token.split("-")[1]
+        i = 0
+        while i < token.length and is_numeric? token[i]
+            i += 1
+        end
+
+        token = token[i..token.length - 1]
+
+        if token.count('_') != 2
+            return false
+        end
+
+        token = token.split("_")[0]
+
+        last = token.length - 1
+        while last >= 0 and is_numeric? token[last]
+            last -= 1
+        end
+
+        token = token[0..last]
+        @gene_name = token
+
+        true
     end
 end
