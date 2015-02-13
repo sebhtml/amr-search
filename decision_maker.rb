afbb2fae1da7f640717f61baee3ee2d8bca8849f
Sébastien Boisvert <boisvert@anl.gov>
2015-02-13 13:10:25 -0600

    add test for probes in v2 and v3

--- decision_maker.rb
+++ decision_maker.rb
@@ -1,21 +1,9 @@
 
+require_relative 'microarray'
 require 'csv'
 
 class DecisionMaker
-    def initialize
-        @feature_counts = {}
-        @probes = {}
-    end
+    def initialize microarray, data
 
-    def load_alignment_data file
-        CSV.foreach(file) do |row|
-            feature_name = row[1]
-        end
-    end
-
-    def load_microarray_probe_data file
-        CSV.foreach(file) do |row|
-            puts row
-        end
     end
 end
--- run_unit_tests.rb
+++ run_unit_tests.rb
@@ -26,6 +26,15 @@ class MyTestCase < Test::Unit::TestCase
         assert_equal 2240, microarray.feature_count
     end
 
+    def load_microarray_v2_v3
+
+        microarray = Microarray.new
+        microarray.load_from_file "Microarray/ARDMv2.csv"
+        microarray.load_from_file "Microarray/ARDMv3-delta.csv"
+
+        assert_equal 2 * 2240, microarray.feature_count
+    end
+
     def foo
         assert_equal(1, 2)
     end
@@ -38,5 +47,6 @@ end
 suite = Test::Unit::TestSuite.new "Test 1"
 suite << MyTestCase.new("load_microarray_v2")
 suite << MyTestCase.new("load_microarray_v3")
+suite << MyTestCase.new("load_microarray_v2_v3")
 
 Test::Unit::UI::Console::TestRunner.run(suite)
