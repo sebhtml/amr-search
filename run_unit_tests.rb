35b98c3aff05f1326630482834856705a1571ed9
Sébastien Boisvert <boisvert@anl.gov>
2015-02-13 17:00:33 -0600

    add test for probe name parser

--- run_unit_tests.rb
+++ run_unit_tests.rb
@@ -39,6 +39,19 @@ class MyTestCase < Test::Unit::TestCase
         assert_equal(1, 2)
     end
 
+    def get_genes
+        tests = [
+                    ["M55547:2314-3138blaOXA-9825_17_51", "blaOXA-9"],
+                    ["X02588:331-1113ant(9)-Ia783_261_299", "ant(9)-Ia"],
+                    ["AF189721:274-1149blaCTX-M-8876_577_611", "blaCTX-M-8"]
+                ]
+        tests.each do |probe_name, gene_name|
+            feature = Feature.new 1, 1, probe_name, "ATCG"
+            assert_equal gene_name, feature.gene
+        end
+
+    end
+
     def teardown
 
     end
@@ -48,5 +61,6 @@ suite = Test::Unit::TestSuite.new "Test 1"
 suite << MyTestCase.new("load_microarray_v2")
 suite << MyTestCase.new("load_microarray_v3")
 suite << MyTestCase.new("load_microarray_v2_v3")
+suite << MyTestCase.new("get_genes")
 
 Test::Unit::UI::Console::TestRunner.run(suite)
