eb00ce1d8e5630bf62dae7aedb6ea0bbb219f3b8
Sébastien Boisvert <boisvert@anl.gov>
2015-03-03 12:56:46 -0600

    add code to download metadata

--- download_metagenomes.py
+++ download_metagenomes.py
@@ -3,13 +3,24 @@
 import requests
 import urllib
 import sys
+import os
+import logging
+import json
+
+project_name = "mgp385"
 
 class MetagenomeProject:
     def __init__(self, name):
+        logging.basicConfig(level=logging.DEBUG)
         self.name = name
         self.positive_filter_list = None
         self.negative_filter_list = None
         self.selected_stage = None
+
+        # check Python version
+        if sys.version_info < (2.7):
+            print("This software needs at l=east Python 2.7.")
+            sys.exit()
 
     def download(self):
 
@@ -76,14 +87,53 @@ class MetagenomeProject:
     def set_stage(self, stage_name):
         self.selected_stage = stage_name
 
-def main():
+    def download_metagenome_metadata(self):
 
-    # check Python version
-    if sys.version_info < (2.7):
-        print("This software needs at l=east Python 2.7.")
-        sys.exit()
+        # see http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1004008
 
-    name = "mgp385"
+        project_api_call = "http://api.metagenomics.anl.gov/project/{}?verbosity=full"
+        download_metagenome_api_call = "http://api.metagenomics.anl.gov/metagenome/{}"
+
+        project = self.name
+
+        address = project_api_call.format(project)
+
+        request = requests.get(address)
+        code = request.status_code
+
+        metadata_directory = project + "_metagenome_metadata"
+
+        if not os.path.exists(metadata_directory):
+            os.makedirs(metadata_directory)
+
+        if code != 200:
+            raise Exception("Error in HTTP return code.")
+
+        project_data = request.json()
+
+        for metagenome in project_data["metagenomes"]:
+            metagenome_name = metagenome[0]
+            address = download_metagenome_api_call.format(metagenome_name)
+            request = requests.get(address)
+
+            code = requests.status_code
+
+            logging.debug("Fetching {} at {}".format(metagenome_name, address))
+            
+            if code != 200:
+                raise Exception("Error in HTTP return code !")
+
+            json_data = request.json()
+            
+            data = json_data
+                    
+            output = open(os.path.join(metadata_directory, metagenome_name + ".json"), "w+")
+            output.write(json.dumps(data))
+            output.close()
+
+def download_data():
+
+    name = project_name
     project = MetagenomeProject(name)
 
     file_with_sample_list = "samples-with-fna-format.txt"
@@ -94,6 +144,13 @@ def main():
 
     arguments = sys.argv
     
+def download_metadata():
+    name = project_name
+    project = MetagenomeProject(name)
+    project.download_metagenome_metadata()
+   
+def main():
+    download_metadata()
 
 if __name__ == '__main__':
     main()
