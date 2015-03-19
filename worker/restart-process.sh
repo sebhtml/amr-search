e38e5e0f8f985e3058271fc8fc31a97aa6fed06e
Sébastien Boisvert <boisvert@anl.gov>
2015-03-19 14:16:32 +0000

    update code before restarting

--- worker/restart-process.sh
+++ worker/restart-process.sh
@@ -4,4 +4,5 @@ cd /mnt/worker/
 source ardm-virtualenv/bin/activate
 killall python
 cd amr-search/
+git pull origin master
 ./data_fetcher/analysis_engine.py run-daemon &> log-$(date +%Y-%m-%d-%H-%M-%S) &
